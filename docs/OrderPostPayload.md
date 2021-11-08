5159c7d75f1157226307e3802cbbec3c083bf918
GitHub Action <action@github.com>
2021-11-08 14:49:07 +0000

    update sdk

--- docs/OrderPostPayload.md
+++ docs/OrderPostPayload.md
@@ -6,7 +6,6 @@ Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **fulfillment_status** | Option<**String**> | The fulfillment status of the order. Defaults to \"UNFULFILLED\". | [optional]
 **internal_notes** | Option<**String**> | Internal notes that will be attached to the order. Viewable only by the team. | [optional]
-**payment_status** | Option<**String**> | The payment status of the order. Defaults to \"UNPAID\".  | [optional]
 **address_id** | Option<**String**> | ID of the address to associate with the order. UUID Version 4. | [optional]
 **customer_id** | Option<**String**> | ID of the customer to associate with the order. UUID Version 4. | [optional]
 **notify** | Option<**bool**> | Indicates if the customer and creator notifications should be sent when creating the order. Requires an address and customer to be set in order for the notifications to be sent. | [optional]
--- src/models/order_post_payload.rs
+++ src/models/order_post_payload.rs
@@ -16,9 +16,6 @@ pub struct OrderPostPayload {
     /// Internal notes that will be attached to the order. Viewable only by the team.
     #[serde(rename = "internal_notes", skip_serializing_if = "Option::is_none")]
     pub internal_notes: Option<String>,
-    /// The payment status of the order. Defaults to \"UNPAID\". 
-    #[serde(rename = "payment_status", skip_serializing_if = "Option::is_none")]
-    pub payment_status: Option<PaymentStatus>,
     /// ID of the address to associate with the order. UUID Version 4.
     #[serde(rename = "address_id", skip_serializing_if = "Option::is_none")]
     pub address_id: Option<String>,
@@ -36,7 +33,6 @@ impl OrderPostPayload {
         OrderPostPayload {
             fulfillment_status: None,
             internal_notes: None,
-            payment_status: None,
             address_id: None,
             customer_id: None,
             notify: None,
@@ -51,13 +47,5 @@ pub enum FulfillmentStatus {
     FULFILLED,
     #[serde(rename = "UNFULFILLED")]
     UNFULFILLED,
-}
-/// The payment status of the order. Defaults to \"UNPAID\". 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
-pub enum PaymentStatus {
-    #[serde(rename = "PAID")]
-    PAID,
-    #[serde(rename = "UNPAID")]
-    UNPAID,
 }
 
